0ef33f360ecd9e21aa43a7eeb67d0ec365617b02
Dron <eptit@mail.ru>
2022-12-18 16:43:48 +0300

    add task1

--- 4.py
+++ 4.py
@@ -5,15 +5,30 @@
 # - 1 -> x > 0, y > 0
 # - 8 -> нет такой четверти
 
-num = int(input("Введите номер четверти - "))
+# num = int(input("Введите номер четверти - "))
 
-if num == 1:
-    print("x > 0, y > 0")
-elif num == 2:
-    print("x < 0, y > 0")
-elif num == 3:
-    print("x < 0, y < 0")
-elif num == 4:
-    print("x > 0, y < 0")
-else:
-    print("Такой четверти нет")
+# if num == 1:
+#     print("x > 0, y > 0")
+# elif num == 2:
+#     print("x < 0, y > 0")
+# elif num == 3:
+#     print("x < 0, y < 0")
+# elif num == 4:
+#     print("x > 0, y < 0")
+# else:
+#     print("Такой четверти нет")
+
+
+quarter = input()
+
+match quarter:
+    case "1":
+        print("x > 0, y > 0")
+    case "2":
+        print("x < 0, y > 0")
+    case "3":
+        print("x < 0, y < 0")
+    case "4":
+        print("x > 0, y < 0")
+    case _:
+        print("error")
